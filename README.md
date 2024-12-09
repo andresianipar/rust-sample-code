10d9932333792598e383cf5b878f7994a4ef9f46
Andre Sianipar <afps9895@gmail.com>
2024-12-09 13:17:17 +0000

    Restructure the project directory to make room for future modules

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Rust Sample Code
 
-This repository serves as documentation for the code I wrote while learning Rust. The sample code is taken from [The Rust Programming Language, 2nd Edition](https://www.amazon.com/Rust-Programming-Language-2nd/dp/1718503105/ref=sr_1_1?qid=1689058222&refinements=p_27%3ASteve+Klabnik&s=books&sr=1-1&text=Steve+Klabnik).
+This repository serves as documentation for the code I wrote while learning Rust. The sample code is taken from:
+1. [The Rust Programming Language, 2nd Edition](https://www.amazon.com/Rust-Programming-Language-2nd/dp/1718503105/ref=sr_1_1?qid=1689058222&refinements=p_27%3ASteve+Klabnik&s=books&sr=1-1&text=Steve+Klabnik)
+99. [The Rust Official Documentation](https://doc.rust-lang.org/)
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,19 @@
-mod R02_programming_a_guessing_game;
-mod R03_common_programming_concepts;
+use R01_the_rust_programming_language::{
+    R02_programming_a_guessing_game::f2, R03_common_programming_concepts::f3,
+};
+
+mod R01_the_rust_programming_language;
+mod R99_rust_official_documentation;
 
 fn main() {
+    // 1 the rust programming language
+    // f1();
+}
+
+fn f1() {
     // 2 programming a guessing game
-    f2();
+    // f2();
 
     // 3 common programming concepts
-    f3();
-}
-
-fn f2() {
-    // 2 programming a guessing game > processing a guest
-    // R02_programming_a_guessing_game::R02_processing_a_guess::f2_0();
-    // R02_programming_a_guessing_game::R02_processing_a_guess::f2_4();
-
-    // 2 programming a guessing game > generating a secret number
-    // R02_programming_a_guessing_game::R03_generating_a_secret_number::f3_2();
-
-    // 2 programming a guessing game > comparing the guess to the secret number
-    // R02_programming_a_guessing_game::R04_comparing_the_guess_to_the_secret_number::f4_0();
-
-    // 2 programming a guessing game > allowing multiple guesses with looping
-    // R02_programming_a_guessing_game::R05_allowing_multiple_guesses_with_looping::f5_0();
-    // R02_programming_a_guessing_game::R05_allowing_multiple_guesses_with_looping::f5_1();
-    // R02_programming_a_guessing_game::R05_allowing_multiple_guesses_with_looping::f5_2();
-}
-
-fn f3() {
-    // 3 common programming concepts > variables and mutability
-    // R03_common_programming_concepts::R01_variables_and_mutability::f1_0();
-    // R03_common_programming_concepts::R01_variables_and_mutability::f1_2();
-
-    // 3 common programming concepts > data types
-    // R03_common_programming_concepts::R02_data_types::f2_1_3();
-    // R03_common_programming_concepts::R02_data_types::f2_1_4();
-    // R03_common_programming_concepts::R02_data_types::f2_1_5();
-    // R03_common_programming_concepts::R02_data_types::f2_2_1();
-    // R03_common_programming_concepts::R02_data_types::f2_2_2();
-    // R03_common_programming_concepts::R02_data_types::f2_2_3();
-    // R03_common_programming_concepts::R02_data_types::f2_2_4();
-
-    // 3 common programming concepts > functions
-    // R03_common_programming_concepts::R03_functions::f3_1_2();
-    // R03_common_programming_concepts::R03_functions::f3_1_3();
-
-    // 3 common programming concepts > Control Flow
-    // R03_common_programming_concepts::R05_control_flow::f5_1_2();
-    // R03_common_programming_concepts::R05_control_flow::f5_2_2();
-    // R03_common_programming_concepts::R05_control_flow::f5_2_3();
-    // R03_common_programming_concepts::R05_control_flow::f5_2_4();
-    // R03_common_programming_concepts::R05_control_flow::f5_2_5();
+    // f3();
 }
